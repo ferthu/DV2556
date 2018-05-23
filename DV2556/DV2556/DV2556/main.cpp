4a12dbd9646e951655f7f8523afd1efed3188932
Unknown <mick3_70@hotmail.com>
2018-05-23 12:41:41 +0200

    Added save result to file

--- DV2556/DV2556/DV2556/main.cpp
+++ DV2556/DV2556/DV2556/main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 
 float calcHitrate(std::vector<IntersectionResult> &res)
@@ -17,6 +18,15 @@ float calcHitrate(std::vector<IntersectionResult> &res)
 			sum += 1;
 	}
 	return sum / ((float)res.size());
+}
+
+void saveTestResult(TestResult &res, std::string fileName)
+{
+	std::fstream fs;
+	fs.open(fileName, std::fstream::out);
+	fs << "#\tHitrate\tDuration" << std::endl;
+	fs << "1\t" << calcHitrate(res.intersectionResults) << "\t" << res.duration << std::endl;
+	fs.close();
 }
 
 
@@ -34,6 +44,6 @@ int main()
 	TestResult resMoller = moller.runTest(&data);
 	
 	printf("Moller\n Hitrate: %f\n Time: %f s\n\ns", calcHitrate(resMoller.intersectionResults), resMoller.duration);
-
+	saveTestResult(resMoller, "MöllerResult.txt");
 	getchar();
 }
